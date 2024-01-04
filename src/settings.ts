d5a9ef56d683927e5feb1f04eebbcb46f88da8ba
Emad <emxdahmad@gmail.com>
2024-01-05 01:57:37 +0530

    add save and usability features (#18)
    * added features
    
    * minor foldercheck fix

--- src/settings.ts
+++ src/settings.ts
@@ -3,13 +3,17 @@ import RecipeGrabber from "./main";
 import * as c from "./constants";
 
 export interface PluginSettings {
-	debug: boolean;
+	folder: string;
+	saveInActiveFile: boolean;
 	recipeTemplate: string;
+	debug: boolean;
 }
 
 export const DEFAULT_SETTINGS: PluginSettings = {
-	debug: false,
+	folder: "",
+	saveInActiveFile: false,
 	recipeTemplate: c.DEFAULT_TEMPLATE,
+	debug: false,
 };
 
 export class SettingsTab extends PluginSettingTab {
@@ -24,25 +28,44 @@ export class SettingsTab extends PluginSettingTab {
 		const { containerEl } = this;
 		containerEl.empty();
 		containerEl.addClass("settingsTemplate");
-
+		
 		new Setting(containerEl)
-			.setName("Debug mode")
-			.setDesc("Just adds some things to make dev life a little easier.")
-			.addToggle((toggle) => {
-				toggle
-					.setValue(this.plugin.settings.debug)
+		.setName("Recipe save folder")
+		.setDesc("Default recipe import location. If empty, recipe will be imported in the Vault root.")
+			.addText((text) => {
+				text
+					.setPlaceholder("eg: Recipes")
+					.setValue(this.plugin.settings.folder)
 					.onChange(async (value) => {
-						this.plugin.settings.debug = value;
+						this.plugin.settings.folder = value;
 						await this.plugin.saveSettings();
 					});
 			});
 
 		new Setting(containerEl)
+			.setName("Save in currently opened file")
+			.setDesc("Imports the recipe into an active document. if no active document, the above save folder setting will apply.")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.saveInActiveFile)
+					.onChange(async (value) => {
+						this.plugin.settings.saveInActiveFile = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		const templateDescription = document.createDocumentFragment();
+		templateDescription.append('Here you can edit the Template for newly created files. See ',
+		templateDescription.createEl("a", {
+			href: "https://github.com/seethroughdev/obsidian-recipe-grabber/blob/master/README.md",
+			text: 'README',
+		}),
+		' for more info.')
+
+		new Setting(containerEl)
 			.setClass("settingsTemplateRow")
 			.setName("Recipe template")
-			.setDesc(
-				"A Handlebars template to render the recipe. (see README for more info)."
-			)
+			.setDesc(templateDescription)
 			.addButton((btn) =>
 				btn
 					.setButtonText("Reset to default")
@@ -63,5 +86,17 @@ export class SettingsTab extends PluginSettingTab {
 					}
 				);
 			});
+
+			new Setting(containerEl)
+				.setName("Debug mode")
+				.setDesc("Just adds some things to make dev life a little easier.")
+				.addToggle((toggle) => {
+					toggle
+						.setValue(this.plugin.settings.debug)
+						.onChange(async (value) => {
+							this.plugin.settings.debug = value;
+							await this.plugin.saveSettings();
+						});
+				});
 	}
 }
